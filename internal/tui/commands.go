29fe48d21a572404f55ac3065d568f1e6dd8a91c
Ignoramuss <mayankdighe@gmail.com>
2025-12-03 10:03:11 -0800

    feat: upgrade Bubble Tea from v1 to v2
    Migrates from Bubble Tea v1.3.10 to v2.0.0-rc.2
    Updates bubbles from v0.21.0 to v2.0.0-rc.1
    Updates lipgloss from v1.1.0 to v2.0.0-beta.3
    
    This upgrade enables the use of bubbleterm, a terminal emulator
    component that requires Bubble Tea v2. Bubbleterm will be used to
    implement an embedded shell feature for pods/containers.
    
    Key API changes:
    - View() now returns tea.View instead of string
    - Screen/mouse config moved to tea.View struct properties
    - AdaptiveColor now uses compat.AdaptiveColor
    - TextInput width uses SetWidth() method

--- internal/tui/commands.go
+++ internal/tui/commands.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 	"text/template"
 
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 	"github.com/samber/lo"
 	"github.com/shvbsle/k10s/internal/k8s"
 	"github.com/shvbsle/k10s/internal/log"
